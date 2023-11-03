21135af22d1ee84e6249e7ff355627b9b2c6af10
Merlinkk <anshulkashyap2504@gmail.com>
2023-11-03 10:30:28 +0530

    javascript done

--- game.js
+++ game.js
@@ -1,5 +1,62 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+const number1 = document.getElementById('number1')
+const number2 = document.getElementById('number2')
 
+function numberReset(){
+    let num1 = Math.ceil(Math.random() * 100)
+    let num2 = Math.ceil(Math.random() * 100)
+
+    number1.textContent = num1
+    number2.textContent = num2
+    console.log(score)
+    timer.textContent = 5
+    resetCount+=1
+    if(resetCount == 15){
+        localStorage.setItem('score',score)
+        window.location.href = "./gameover.html"
+    }
+}
+let score = 0
+let resetCount = 0
 // Iteration 3: Make the options button functional
 
+const greaterThan = document.getElementById('greater-than') 
+const equalTo = document.getElementById('equal-to') 
+const lowerThan = document.getElementById('lesser-than') 
+
+greaterThan.onclick = () =>{
+    if(number1.textContent  >
+        number2.textContent){
+        score+=1
+    }
+    numberReset()
+}
+
+lowerThan.onclick = () =>{
+    if(number1.textContent  <
+        number2.textContent){
+        score+=1
+    }
+    numberReset()
+}
+
+equalTo.onclick = () =>{
+    if(number1.textContent  ==
+        number2.textContent){
+        score+=1
+    }
+    numberReset()
+}
+
 // Iteration 4: Build a timer for the game
+const timer = document.getElementById('timer')
+timer.textContent = 5
+
+const timeDecrement  = setInterval(() => {
+    timer.textContent -=1
+    if(timer.textContent == 0){
+        numberReset()
+        timer.textContent = 5
+    }
+}, 1000);
+
--- score.js
+++ score.js
@@ -1,1 +1,14 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score = localStorage.getItem('score')
+
+const scoreBoard = document.getElementById('score-board')
+
+window.onload = () =>{
+    scoreBoard.textContent = score
+}
+
+const playAgain = document.getElementById('play-again-button')
+
+playAgain.onclick = () =>{
+    window.location.href = "./game.html"
+}
